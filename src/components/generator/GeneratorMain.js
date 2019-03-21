c0bb132b9cc58e351ae0594cbb8a7af0adba7c72
inari a <iaaltojarvi@gmail.com>
2019-03-21 16:25:31 +0100

    generaattorissa napit ja haut kategorioittain

--- src/components/generator/GeneratorMain.js
+++ src/components/generator/GeneratorMain.js
@@ -6,21 +6,35 @@ class Generator extends Component {
     constructor() {
         super()
         this.state = {
-            quotes: []
+            quotes: [],
+            isChecked: true,
         }
-        this.getQuotes = this.getQuotes.bind(this);
+        this.getQuotes1 = this.getQuotes1.bind(this);
+        this.getQuotes2 = this.getQuotes2.bind(this);
+        this.getQuotes3 = this.getQuotes3.bind(this);
     }
-
-    getQuotes() {
-        fetch('https://hattu-server.herokuapp.com/api/generaattori', {mode: 'cors'})
+   
+    getQuotes1() {
+        fetch('https://hattu-server.herokuapp.com/api/generaattori/:ajatus', { mode: 'cors' })
             .then(res => res.json())
             .then(data => this.setState({ quotes: data }))
-            .catch((err) => {throw err})
+            .catch((err) => { throw err })
     }
-
+    getQuotes2() {
+        fetch('https://hattu-server.herokuapp.com/api/generaattori/:tsemppi', { mode: 'cors' })
+            .then(res => res.json())
+            .then(data => this.setState({ quotes: data }))
+            .catch((err) => { throw err })
+    }
+    getQuotes3() {
+        fetch('https://hattu-server.herokuapp.com/api/generaattori/:kiroilu', { mode: 'cors' })
+            .then(res => res.json())
+            .then(data => this.setState({ quotes: data }))
+            .catch((err) => { throw err })
+    }
     render() {
         console.log(this.state.quotes)
-        const getSentence = this.state.quotes.map((powerQuote, i)=>
+        const getSentence = this.state.quotes.map((powerQuote, i) =>
             <p key={i}>{powerQuote.sentence}</p>
         );
         return (
@@ -29,11 +43,16 @@ class Generator extends Component {
                 <h1 className="title">Voimalausegeneraattori</h1>
                 <hr className="dash"></hr>
                 <div className="app">
-
                     <div className="btn">
-                        <button className="btn-quote" onClick={this.getQuotes}>
-                            Hae sitaatti
+                        <button className="btn-quote-ajatus" onClick={this.getQuotes1}>
+                        Ajatus
                         </button>
+                        <button className="btn-quote-tsemppi" onClick={this.getQuotes2}>
+                        Tsemppi
+                        </button> 
+                        <button className="btn-quote-kiroilu" onClick={this.getQuotes3}>
+                        Kiroilu
+                        </button> 
                     </div>
                     <div className="quote">
                         <p className="quote_text">{getSentence}</p>
