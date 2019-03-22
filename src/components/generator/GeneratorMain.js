54e18779646a70792553f3b054d3f61cb47c6802
inari a <iaaltojarvi@gmail.com>
2019-03-22 11:01:14 +0100

    generaattorisivua tuunattu

--- src/components/generator/GeneratorMain.js
+++ src/components/generator/GeneratorMain.js
@@ -15,7 +15,7 @@ class Generator extends Component {
         this.getQuotes2 = this.getQuotes2.bind(this);
         this.getQuotes3 = this.getQuotes3.bind(this);
     }
-   
+
     getQuotes1() {
         fetch('https://hattu-server.herokuapp.com/api/generaattori/:ajatus', { mode: 'cors' })
             .then(res => res.json())
@@ -40,27 +40,31 @@ class Generator extends Component {
             <p key={i}>{powerQuote.sentence}</p>
         );
         return (
-           
+
             <div className="content">
-                <h1 className="title">Voimalausegeneraattori</h1>
+                <h1 className="title">Feministinen generaattori</h1>
                 <hr className="dash"></hr>
+                <p className = "kuvailu">
+                Iskikö huijarisyndrooma? Kiroiluttaako, mutta et pääse alkuun? Generoi alta
+                itsellesi tilanteeseen sopiva feministinen lause tai päästele ärräpäitä generaattorin
+                avustuksella.</p>
                 <div className="app">
                     <div className="btn">
                         <button className="btn-quote-ajatus" onClick={this.getQuotes1}>
-                        Ajatus
+                            Ajatus
                         </button>
                         <button className="btn-quote-tsemppi" onClick={this.getQuotes2}>
-                        Tsemppi
-                        </button> 
+                            Tsemppi
+                        </button>
                         <button className="btn-quote-kiroilu" onClick={this.getQuotes3}>
-                        Kiroilu
-                        </button> 
+                            Kiroilu
+                        </button>
                     </div>
                     <Container className="puhekupla">
-                    <img src={image} alt="women" width="550"/>
-                    <div className="quote">
-                        <p className="quote_text">{getSentence}</p>
-                    </div><br></br>
+                        <img src={image} alt="women" height="650" />
+                        <div className="quote">
+                            <p className="quote_text">{getSentence}</p>
+                        </div><br></br>
                     </Container>
                     <p className="copy">Image created by rawpixel.com - www.freepik.com</p>
                 </div>
