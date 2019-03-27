4c2598bafba61d9c21ff9e9ecd889ee0b8da250f
MariaKau <mariakauppi1@gmail.com>
2019-03-27 15:42:29 +0200

    bubble animation

--- src/components/generator/Bubble.js
+++ src/components/generator/Bubble.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import './GeneratorMain.css';
-
+import './Bubble.css';
+import {Animated} from "react-animated-css";
 
 const Bubble = (props) =>
 <p id="SpeechBubble" className={props.animate ? 'AnimatingBubble':''}
     onAnimationEnd={()=>{props.doneAnimating()}}
-    >{props.value.sentence}</p>
+    ><Animated animationIn="heartBeat" className="animated">{props.value.sentence}</Animated></p>
 
 
 export default Bubble
