a4b31e69bfcd7729435658cab631bd71f57450bd
rinskinen <riina.j.moilanen@gmail.com>
2019-03-27 15:38:03 +0200

    Bingo line is cocktail-pink

--- src/components/bingo/Square.js
+++ src/components/bingo/Square.js
@@ -2,24 +2,13 @@ import React from 'react';
 
 class Square extends React.Component {
 
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      white: this.props.white
-    }
-  }
-
-  changeColor = () => {
-    // this.setState({ white: !this.state.white });
+  checkIfBingo = () => {
     this.props.checkBingo(this.props.number);
   }
 
   render() {
-    // let square = this.state.white ? "whiteButton" : "orangeButton";
-
     return (
-      <button className={this.props.className} id="square" onClick={this.changeColor}>
+      <button className={this.props.white ? 'whiteButton' : this.props.bingoSquare ? 'bingoButton' : 'orangeButton'} id="square" onClick={this.checkIfBingo}>
         {this.props.quote}
       </button>
     )
