c5ba3d25190eeef979020669630c66edeb3f2082
rinskinen <riina.j.moilanen@gmail.com>
2019-03-28 13:23:01 +0200

    Bingo is smaller

--- src/components/bingo/Board.js
+++ src/components/bingo/Board.js
@@ -101,12 +101,6 @@ class Board extends React.Component {
       if (this.state.bingoLines.includes(i)) continue;
       const [a, b, c, d] = lines[i];
       if (this.state.squares.includes(this.state.quotes[a].id) && this.state.squares.includes(this.state.quotes[b].id) && this.state.squares.includes(this.state.quotes[c].id) && this.state.squares.includes(this.state.quotes[d].id)) {
-        // if (this.state.bingoLines.includes(i)) {
-        //   this.setState({
-        //     bingoLines: this.state.bingoLines.filter(function (bline) {
-        //       bline !== i
-        //     })
-        //   })
         return this.setState({
           bingoFound: true,
           show: true
@@ -138,12 +132,11 @@ class Board extends React.Component {
     const handleHide = () => this.setState({ show: false });
     return (
       <div>
-        <br />
         <Alert show={this.state.show} variant="success" id="alert" >
           <Alert.Heading>BINGO!</Alert.Heading>
           <p>Ohhoh, taidat olla aikamoisen setämiehen seurassa!
           </p>
-          <br /><div className="d-flex justify-content-end">
+          <div className="d-flex justify-content-end">
             <Button onClick={handleHide} variant="outline-success" id="bingobutton">
               Sulje
             </Button>
